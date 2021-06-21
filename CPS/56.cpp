d2e815d5521f86c17cb7d00ebc57d0ab20658c6c
Lee Na Ryeong <lnr99207@naver.com>
2021-06-21 16:07:43 +0900

    Add files via upload

---
+++ CPS/56.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+//재귀 가지(?)를 뻗어나가다가 최종 목적지에 도착했는지?!(if-else 형태 이용) 
+void recursion(int x){
+	if(x==0){ //종료지점->더이상 호출하지 않고 함수 종료! 
+		return;
+	}else{ //STACK 자료구조 사용. 
+		recursion(x-1);
+		printf("%d ", x);
+	}
+}
+
+int main(){
+	freopen("input.txt","rt",stdin);
+	int n;
+	scanf("%d", &n);
+	recursion(n);
+	
+	return 0;
+}
