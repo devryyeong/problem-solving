607f74bac2878133115aee62b81a1084a3d36d07
Lee Na Ryeong <lnr99207@naver.com>
2021-09-11 16:15:56 +0900

    Add files via upload

---
+++ BOJ/1001.cpp
@@ -0,0 +1,18 @@
+//1001] A-B
+#include <iostream>
+using namespace std;
+
+int n, k;
+int num[1049000], res[1049000];
+
+void Solve(int a, int b){
+  printf("%d", a-b);
+}
+
+int main(){
+  int a, b;
+  scanf("%d %d", &a, &b);
+  Solve(a, b);
+
+  return 0;
+}
