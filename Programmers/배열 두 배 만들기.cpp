50fd7f91c1aacc6a5038f90b1bff405d36d8442d
Na Ryeong Lee <lnr99207@naver.com>
2022-10-19 23:06:39 +0900

    Add files via upload

---
+++ Programmers/배열 두 배 만들기.cpp
@@ -0,0 +1,15 @@
+// 배열 두배 만들기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    for(auto a:numbers){
+        answer.push_back(a*2);
+    }
+    
+    return answer;
+}
